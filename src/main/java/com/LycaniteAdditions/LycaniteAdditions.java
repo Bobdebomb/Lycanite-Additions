b2ca5f3c2e7c21d39ecaf49a94cdd4f4a1028920
Bobdebomb <134546356+Bobdebomb@users.noreply.github.com>
2025-11-30 20:14:49 -0500

    fangtusk stuff added and allat not spawning in game tho

--- src/main/java/com/LycaniteAdditions/LycaniteAdditions.java
+++ src/main/java/com/LycaniteAdditions/LycaniteAdditions.java
@@ -1,4 +1,4 @@
-package com.example.modid;
+package com.LycaniteAdditions;
 
 import com.example.modid.Tags;
 import net.minecraftforge.fml.common.Mod;
@@ -7,7 +7,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 @Mod(modid = Tags.MOD_ID, name = Tags.MOD_NAME, version = Tags.VERSION)
-public class ExampleMod {
+public class LycaniteAdditions {
 
     public static final Logger LOGGER = LogManager.getLogger(Tags.MOD_NAME);
 
@@ -19,6 +19,8 @@ public class ExampleMod {
     @Mod.EventHandler
     public void preInit(FMLPreInitializationEvent event) {
         LOGGER.info("Hello From {}!", Tags.MOD_NAME);
+
+
     }
 
 }
