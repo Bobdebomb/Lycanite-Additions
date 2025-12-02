8303196791bd3aefe9cc1a95956c88aaa397e124
Bobdebomb <134546356+Bobdebomb@users.noreply.github.com>
2025-12-01 21:26:55 -0500

    fangtusk stuff added and allat not spawning in game tho

---
+++ src/main/java/com/lycaniteadditions/core/entity/creature/entitylushrak.java
@@ -0,0 +1,116 @@
+package com.lycaniteadditions.core.entity.creature;
+
+import com.lycanitesmobs.core.entity.TameableCreatureEntity;
+import com.lycanitesmobs.core.entity.goals.actions.AttackMeleeGoal;
+import com.lycanitesmobs.core.info.ObjectLists;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.EnumCreatureAttribute;
+import net.minecraft.entity.monster.IMob;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.MobEffects;
+import net.minecraft.item.ItemStack;
+import net.minecraft.potion.PotionEffect;
+import net.minecraft.util.DamageSource;
+import net.minecraft.util.EnumHand;
+import net.minecraft.world.World;
+
+public class entitylushrak extends TameableCreatureEntity implements IMob {
+
+    // ==================================================
+    //                    Constructor
+    // ==================================================
+    public entitylushrak(World world) {
+        super(world);
+
+        // Setup:
+        this.attribute = EnumCreatureAttribute.UNDEFINED;
+        this.spawnsUnderground = true;
+        this.hasAttackSound = true;
+        this.spreadFire = true;
+
+        this.canGrow = true;
+        this.babySpawnChance = 0.01D;
+        this.setupMob();
+    }
+
+    // ========== Init AI ==========
+    @Override
+    protected void initEntityAI() {
+        super.initEntityAI();
+        this.tasks.addTask(this.nextCombatGoalIndex++, new AttackMeleeGoal(this).setTargetClass(EntityPlayer.class).setLongMemory(false));
+        this.tasks.addTask(this.nextCombatGoalIndex++, new AttackMeleeGoal(this));
+    }
+
+
+    // ==================================================
+    //                      Updates
+    // ==================================================
+    // ========== Living Update ==========
+    @Override
+    public void onLivingUpdate() {
+        super.onLivingUpdate();
+
+        // Water Healing:
+        if(this.getAir() >= 0) {
+            if (this.isInWater())
+                this.addPotionEffect(new PotionEffect(MobEffects.REGENERATION, 3 * 20, 2));
+            else if (this.waterContact())
+                this.addPotionEffect(new PotionEffect(MobEffects.REGENERATION, 3 * 20, 1));
+        }
+    }
+
+
+    // ==================================================
+    //                      Attacks
+    // ==================================================
+    // ========== Melee Attack ==========
+    @Override
+    public boolean attackMelee(Entity target, double damageScale) {
+        if(!super.attackMelee(target, damageScale))
+            return false;
+
+        // Leech:
+        float leeching = this.getEffectStrength(this.getAttackDamage(damageScale) / 2);
+        this.heal(leeching);
+
+        return true;
+    }
+
+
+    // ==================================================
+    //                    Taking Damage
+    // ==================================================
+    // ========== Damage Modifier ==========
+    public float getDamageModifier(DamageSource damageSrc) {
+        if(damageSrc.isFireDamage())
+            return 2.0F;
+        if(damageSrc.getTrueSource() != null) {
+            ItemStack heldItem = ItemStack.EMPTY;
+            if(damageSrc.getTrueSource() instanceof EntityLivingBase) {
+                EntityLivingBase entityLiving = (EntityLivingBase)damageSrc.getTrueSource();
+                if(!entityLiving.getHeldItem(EnumHand.MAIN_HAND).isEmpty()) {
+                    heldItem = entityLiving.getHeldItem(EnumHand.MAIN_HAND);
+                }
+            }
+            if(ObjectLists.isAxe(heldItem)) {
+                return 2.0F;
+            }
+        }
+        return super.getDamageModifier(damageSrc);
+    }
+
+    // ==================================================
+    //                     Equipment
+    // ==================================================
+    @Override
+    public int getNoBagSize() { return 0; }
+    @Override
+    public int getBagSize() { return this.creatureInfo.bagSize; }
+
+
+    // ==================================================
+    //                     Pet Control
+    // ==================================================
+    public boolean petControlsEnabled() { return true; }
+}
---
+++ src/main/java/com/lycaniteadditions/core/entity/creature/entitymycoflawg.java
@@ -0,0 +1,209 @@
+package com.lycaniteadditions.core.entity.creature;
+
+import com.lycanitesmobs.ObjectManager;
+import com.lycanitesmobs.core.entity.RideableCreatureEntity;
+import com.lycanitesmobs.core.entity.goals.actions.AttackMeleeGoal;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.EnumCreatureAttribute;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.MobEffects;
+import net.minecraft.potion.PotionEffect;
+import net.minecraft.world.World;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.entity.player.AttackEntityEvent;
+
+import java.util.List;
+
+public class entitymycoflawg extends RideableCreatureEntity {
+
+    protected boolean leapedAbilityQueued = false;
+    protected boolean leapedAbilityReady = false;
+
+    // ==================================================
+    //                    Constructor
+    // ==================================================
+    public entitymycoflawg(World world) {
+        super(world);
+
+        // Setup:
+        this.attribute = EnumCreatureAttribute.UNDEFINED;
+        this.hasAttackSound = true;
+        this.spreadFire = false;
+
+        this.canGrow = true;
+        this.babySpawnChance = 0.1D;
+        this.setupMob();
+
+        // Stats:
+        this.stepHeight = 1.0F;
+    }
+
+    // ========== Init AI ==========
+    @Override
+    protected void initEntityAI() {
+        super.initEntityAI();
+        this.tasks.addTask(this.nextCombatGoalIndex++, new AttackMeleeGoal(this).setTargetClass(EntityPlayer.class).setLongMemory(false));
+        this.tasks.addTask(this.nextCombatGoalIndex++, new AttackMeleeGoal(this));
+    }
+
+
+    // ==================================================
+    //                      Updates
+    // ==================================================
+    // ========== Living Update ==========
+    @Override
+    public void onLivingUpdate() {
+        super.onLivingUpdate();
+
+        // Random Leaping:
+        if(!this.isTamed() && this.onGround && !this.getEntityWorld().isRemote) {
+            if(this.hasAttackTarget()) {
+                if(this.rand.nextInt(10) == 0)
+                    this.leap(4.0F, 0.5D, this.getAttackTarget());
+            }
+        }
+
+        // Leap Landing Paralysis:
+        if(this.leapedAbilityQueued && !this.onGround && !this.getEntityWorld().isRemote) {
+            this.leapedAbilityQueued = false;
+            this.leapedAbilityReady = true;
+        }
+        if(this.leapedAbilityReady && this.onGround && !this.getEntityWorld().isRemote) {
+            this.leapedAbilityReady = false;
+            double distance = 4.0D;
+            List<EntityLivingBase> possibleTargets = this.getEntityWorld().getEntitiesWithinAABB(EntityLivingBase.class, this.getEntityBoundingBox().grow(distance, distance, distance), possibleTarget -> {
+                if (!possibleTarget.isEntityAlive()
+                        || possibleTarget == entitymycoflawg.this
+                        || entitymycoflawg.this.isRidingOrBeingRiddenBy(possibleTarget)
+                        || entitymycoflawg.this.isOnSameTeam(possibleTarget)
+                        || !entitymycoflawg.this.canAttackClass(possibleTarget.getClass())
+                        || !entitymycoflawg.this.canAttackEntity(possibleTarget))
+                    return false;
+
+                return true;
+            });
+            if(!possibleTargets.isEmpty()) {
+                for(EntityLivingBase possibleTarget : possibleTargets) {
+                    boolean doDamage = true;
+                    if(this.getRider() instanceof EntityPlayer) {
+                        if(MinecraftForge.EVENT_BUS.post(new AttackEntityEvent((EntityPlayer)this.getRider(), possibleTarget))) {
+                            doDamage = false;
+                        }
+                    }
+                    if(doDamage) {
+                        if (ObjectManager.getEffect("weight") != null)
+                            possibleTarget.addPotionEffect(new PotionEffect(ObjectManager.getEffect("weight"), this.getEffectDuration(5), 1));
+                        else
+                            possibleTarget.addPotionEffect(new PotionEffect(MobEffects.SLOWNESS, 10 * 20, 0));
+                    }
+                }
+            }
+            this.playAttackSound();
+        }
+    }
+
+    public void riderEffects(EntityLivingBase rider) {
+        if(rider.isPotionActive(MobEffects.SLOWNESS))
+            rider.removePotionEffect(MobEffects.SLOWNESS);
+        if(rider.isPotionActive(ObjectManager.getEffect("weight")))
+            rider.removePotionEffect(ObjectManager.getEffect("weight"));
+    }
+
+
+    // ==================================================
+    //                      Movement
+    // ==================================================
+    // ========== Movement Speed Modifier ==========
+    @Override
+    public float getAISpeedModifier() {
+        if(!this.onGround)
+            return 2.0F;
+        return 1.0F;
+    }
+
+    // ========== Mounted Offset ==========
+    @Override
+    public double getMountedYOffset() {
+        return (double)this.height * 0.8D;
+    }
+
+    // ========== Leap ==========
+    @Override
+    public void leap(double distance, double leapHeight) {
+        super.leap(distance, leapHeight);
+        if(!this.getEntityWorld().isRemote)
+            this.leapedAbilityQueued = true;
+    }
+
+    // ========== Leap to Target ==========
+    @Override
+    public void leap(float range, double leapHeight, Entity target) {
+        super.leap(range, leapHeight, target);
+        if(!this.getEntityWorld().isRemote)
+            this.leapedAbilityQueued = true;
+    }
+
+
+    // ==================================================
+    //                   Mount Ability
+    // ==================================================
+    public void mountAbility(Entity rider) {
+        if(!this.onGround)
+            return;
+        if(this.abilityToggled)
+            return;
+        if(this.getStamina() < this.getStaminaCost())
+            return;
+
+        this.playJumpSound();
+        this.leap(2.0D, 1.5D);
+
+        this.applyStaminaCost();
+    }
+
+    public float getStaminaCost() {
+        return 15;
+    }
+
+    public int getStaminaRecoveryWarmup() {
+        return 5 * 20;
+    }
+
+    public float getStaminaRecoveryMax() {
+        return 1.0F;
+    }
+
+
+    // ==================================================
+    //                     Equipment
+    // ==================================================
+    @Override
+    public int getNoBagSize() { return 0; }
+    @Override
+    public int getBagSize() { return this.creatureInfo.bagSize; }
+
+
+
+
+    // ==================================================
+    //                     Abilities
+    // ==================================================
+    @Override
+    public boolean canClimb() { return false; }
+
+
+    // ==================================================
+    //                     Pet Control
+    // ==================================================
+    public boolean petControlsEnabled() { return true; }
+
+
+    // ==================================================
+    //                     Immunities
+    // ==================================================
+    @Override
+    public float getFallResistance() {
+        return 20;
+    }
+}
